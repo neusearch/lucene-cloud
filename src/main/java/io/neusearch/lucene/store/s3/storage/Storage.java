dc41143907d4f270858de1e7700ed4fa8858f45e
Steven Kim <steven@wrtn.io>
2023-12-31 17:20:02 +0900

    add configs and comments for publishing to Maven Central

--- src/main/java/io/neusearch/lucene/store/s3/storage/Storage.java
+++ src/main/java/io/neusearch/lucene/store/s3/storage/Storage.java
@@ -6,26 +6,91 @@ import java.io.File;
 import java.io.IOException;
 import java.nio.file.Path;
 
+/**
+ * Interface to storage that durably stores Lucene data
+ */
 public interface Storage {
-    String[] listAll() throws IOException;
+    /**
+     * Lists all the file names within storage
+     *
+     * @return the object names array
+     */
+    String[] listAll();
 
+    /**
+     * Gets file length matched with the provided name
+     * @param name the name of file within the storage
+     * @return the file size in bytes
+     */
     long fileLength(final String name);
 
+    /**
+     * Renames file based on the provided names
+     *
+     * @param from the file name to be renamed from
+     * @param to the file name to be renamed to
+     */
     void rename(final String from, final String to);
 
-    void writeFromFile(final Path filePath) throws IOException;
+    /**
+     * Writes a file to storage using the given file
+     *
+     * @param filePath the absolute file path
+     */
+    void writeFromFile(final Path filePath);
 
+    /**
+     * Deletes a file matched with the provided name
+     *
+     * @param name the name of file in storage
+     */
     void deleteFile(final String name);
 
+    /**
+     * Reads a range of bytes within a file from storage and writes to a file
+     *
+     * @param name the file name
+     * @param fileOffset the range start offset
+     * @param len the length of the range
+     * @param file the File object to be written
+     * @throws IOException if writing to the file failed for reasons
+     */
     void readRangeToFile(final String name, final int fileOffset,
                    final int len, final File file) throws IOException;
 
+    /**
+     * Reads a whole file in storage and writes to the given file
+     *
+     * @param name the file name to read
+     * @param file the File object to be written
+     * @throws IOException if writing to the file failed for reasons
+     */
     void readToFile(final String name, final File file) throws IOException;
 
+    /**
+     * Reads a range of bytes from a file in storage and writes to a specific offset of the given buffer
+     *
+     * @param name the file name in storage
+     * @param buffer the buffer to populate read data
+     * @param bufOffset the start offset inside the buffer
+     * @param fileOffset the start offset inside the file
+     * @param len the length to read from the file
+     * @return the read bytes
+     * @throws IOException if copying into buffer failed for reasons
+     */
     int readBytes(final String name, final byte[] buffer, final int bufOffset,
                   final int fileOffset, final int len) throws IOException;
 
+    /**
+     * Reads all the files in storage and writes to the given directory
+     *
+     * @param dir the directory to write all the files in storage
+     * @param s3Directory the caller's object to access/change the stats of the directory
+     */
     void readAllToDir(final String dir, final S3Directory s3Directory);
 
-    void close() throws IOException;
+    /**
+     * Releases the created storage
+     */
+    void close();
 }
--- src/main/java/io/neusearch/lucene/store/s3/storage/StorageFactory.java
+++ src/main/java/io/neusearch/lucene/store/s3/storage/StorageFactory.java
@@ -6,20 +6,32 @@ import org.slf4j.LoggerFactory;
 import java.io.IOException;
 import java.util.HashMap;
 
+/**
+ * Cloud storage factory class designed to handle various storage types.
+ */
 public class StorageFactory {
     private static final Logger logger = LoggerFactory.getLogger(StorageFactory.class);
-    public Storage createStorage(String storageType, HashMap<String, Object> params) throws IOException
+
+    /**
+     * Creates a new StorageFactory
+     */
+    public StorageFactory() {}
+
+    /**
+     * Creates a specific Storage object based on the passed parameters.
+     *
+     * @param storageType the type of storage to be created, currently only AWS S3 is supported
+     * @param params the parameters map that is passed to a specific Storage implementation
+     * @return the storage object
+     * @throws IllegalArgumentException if the passed storage type is not supported
+     */
+    public Storage createStorage(String storageType, HashMap<String, Object> params) throws RuntimeException
     {
-        try {
-            if (storageType == null || storageType.isEmpty())
-                return null;
-            return switch (storageType.toLowerCase()) {
-                case "s3" -> new S3Storage(params);
-                default -> throw new IllegalArgumentException("Unknown storage " + storageType);
-            };
-        } catch (IOException ioe) {
-            logger.error("{}", ioe.toString());
-            throw ioe;
-        }
+        if (storageType == null || storageType.isEmpty())
+            return null;
+        return switch (storageType.toLowerCase()) {
+            case "s3" -> new S3Storage(params);
+            default -> throw new IllegalArgumentException("Unknown storage " + storageType);
+        };
     }
 }
